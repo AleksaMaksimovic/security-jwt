89a8cfece9096500dbca4cc9af6bf3ed1069ddf0
Aleksa Maksimovic <maksimovic1919@gmail.com>
2022-09-16 09:08:15 +0200

    1

--- src/main/java/rs/gov/mup/securityjwt/config/SecurityConfig.java
+++ src/main/java/rs/gov/mup/securityjwt/config/SecurityConfig.java
@@ -9,11 +9,14 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import javax.servlet.http.HttpServletResponse;
 
 @EnableWebSecurity
 @EnableMethodSecurity
@@ -51,6 +54,15 @@ public class SecurityConfig {
         http.authorizeRequests().anyRequest().authenticated();
         http.addFilterAt(loginFilter, BasicAuthenticationFilter.class);
         http.addFilterAt(jwtFilter, BasicAuthenticationFilter.class);
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.exceptionHandling()
+                .accessDeniedHandler(((request, response, accessDeniedException) -> {
+                    response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+                    response.getWriter().println(accessDeniedException.getMessage());
+                })).authenticationEntryPoint(((request, response, authException) -> {
+                    response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                    response.getWriter().println(authException.getMessage());
+                }));
         return http.build();
     }
 }
